e4ba46414301b2f1ef4c207826bd66181f74fb2b
Rupasappati <rupasappati@gmail.com>
2022-05-11 14:49:12 +0530

    Added: Refractor code

--- Employeewagecomputation/Program.cs
+++ Employeewagecomputation/Program.cs
@@ -10,13 +10,30 @@ namespace Employeewagecomputation
             Console.WriteLine("Welcome to Employee Wage Computation");
 
             Employeewagecomputation objEmp = new Employeewagecomputation();
+
+
+            //Employee Attendance
             objEmp.checkEmpISPresentOrNot();
+
+
+            //Calculate Daily Employee Wage
             objEmp.calcEmpDailywage();
+
+
+            //Calculate Full/PartTime Employee Wage
             objEmp.checkEmpFullOrPartTime();
+
+
+            //Solving using Switch Case
             objEmp.switchcase();
+
+            //Calculating Wages for a month
             objEmp.WagesForMonth();
+            
+           
 
             WagesTillCondition wagesTillCondition = new WagesTillCondition();
+            //Calculating Wages till the condition of 100 hours of month or 20 days…
             wagesTillCondition.GetWageTillCondition();
 
 
